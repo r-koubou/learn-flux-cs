b05df458d9c6f1120670249241692f692a4151d9
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2024-06-23 19:58:43 +0900

    ドキュメントコメント追記

--- learn-flux-cs/Flux/Dispatchers/Extensions/DispatcherExtension.cs
+++ learn-flux-cs/Flux/Dispatchers/Extensions/DispatcherExtension.cs
@@ -3,8 +3,14 @@ using System.Threading.Tasks;
 
 namespace LearnFlux.Flux.Dispatchers.Extensions;
 
+/// <summary>
+/// <see cref="IDispatcher{TPayload}"/> に対する拡張メソッド
+/// </summary>
 public static class DispatcherExtension
 {
+    /// <summary>
+    /// Func・ラムダ式を利用してハンドラを登録する
+    /// </summary>
     public static IDisposable AddHandler<TPayload>( this IDispatcher<TPayload> me, Func<TPayload, Task> handlerAsync )
         => me.AddHandler( new AnonymousHandler<TPayload>( handlerAsync ) );
 }
