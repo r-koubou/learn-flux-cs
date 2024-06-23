beec9385f610035828660b0779e2cd753769322d
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2024-06-23 19:57:36 +0900

    ドキュメントコメント追記

--- learn-flux-cs/Flux/Dispatchers/IDispatchHandler.cs
+++ learn-flux-cs/Flux/Dispatchers/IDispatchHandler.cs
@@ -4,7 +4,18 @@ namespace LearnFlux.Flux.Dispatchers;
 
 public interface IDispatchHandler<in TPayload>
 {
+    /// <summary>
+    /// <see cref="IDispatcher{TPayload}"/> からのコールバックをハンドリングする。
+    /// </summary>
+    /// <remarks>既定では<see cref="HandleAsync"/> をブロッキングして実行する</remarks>
     void Handle( TPayload payload )
         => HandleAsync( payload ).GetAwaiter().GetResult();
+
+    /// <summary>
+    /// <see cref="IDispatcher{TPayload}"/> からのコールバックをハンドリングする。
+    /// </summary>
+    /// <remarks>
+    /// 適宜データ更新や View への通知を行う。
+    /// </remarks>
     Task HandleAsync( TPayload payload );
 }
--- learn-flux-cs/Flux/Dispatchers/IDispatcher.cs
+++ learn-flux-cs/Flux/Dispatchers/IDispatcher.cs
@@ -3,10 +3,26 @@ using System.Threading.Tasks;
 
 namespace LearnFlux.Flux.Dispatchers;
 
+/// <summary>
+/// Dispatcher を表現するインターフェース
+/// </summary>
 public interface IDispatcher<TPayload>
 {
+    /// <summary>
+    /// ディスパッチを受け取るハンドラを追加する
+    /// </summary>
+    /// <returns>ハンドラ登録解除用のトークン</returns>
     IDisposable AddHandler( IDispatchHandler<TPayload> handler );
+
+    /// <summary>
+    /// ディスパッチを行う
+    /// </summary>
+    /// <remarks>既定では<see cref="DispatchAsync"/> をブロッキングして実行する</remarks>
     void Dispatch( TPayload payload )
         => DispatchAsync( payload ).GetAwaiter().GetResult();
+
+    /// <summary>
+    /// ディスパッチを行う
+    /// </summary>
     Task DispatchAsync( TPayload payload );
 }
