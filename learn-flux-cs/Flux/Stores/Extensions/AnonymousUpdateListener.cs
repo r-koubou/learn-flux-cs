9861709dc63aa9282b93c7f1330802e43b2c382f
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2024-06-23 19:49:54 +0900

    リファクタ、ドキュメントコメント追記

--- learn-flux-cs/Flux/Stores/Extensions/AnonymousUpdateListener.cs
+++ learn-flux-cs/Flux/Stores/Extensions/AnonymousUpdateListener.cs
@@ -3,8 +3,15 @@ using System.Threading.Tasks;
 
 namespace LearnFlux.Flux.Stores.Extensions;
 
-public class AnonymousUpdateListener<TPayload>( Func<TPayload, Task> onStoreUpdated ) : IStoreUpdateListener<TPayload>
+/// <summary>
+/// Func・ラムダ式を利用して Store の更新通知を受取可能な匿名リスナー
+/// </summary>
+/// <param name="onValueUpdatedAsync"><see cref="IStoreUpdateListener{TPayload}.OnValueUpdatedAsync"/>に対応する処理</param>
+public class AnonymousUpdateListener<TPayload>( Func<TPayload, Task> onValueUpdatedAsync ) : IStoreUpdateListener<TPayload>
 {
+    ///
+    /// <inheritdoc />
+    ///
     public async Task OnValueUpdatedAsync( TPayload payload )
-        => await onStoreUpdated( payload );
+        => await onValueUpdatedAsync( payload );
 }
